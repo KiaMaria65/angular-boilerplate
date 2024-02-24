2c9b03bd3ea4c3d46479e61c39d5bb07c4a0eafb
KiaMaria65 <summermace454@gmail.com>
2024-02-24 16:17:05 +0800

    added tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
